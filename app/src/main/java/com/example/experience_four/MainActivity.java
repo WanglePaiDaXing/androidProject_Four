329cbb18b842e1d1e5ac3235a6a9674af1aa15ec
WanglePaiDaXing <abc6339139@vip.qq.com>
2019-05-04 11:30:16 +0800

    First Version

---
+++ app/src/main/java/com/example/experience_four/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.experience_four;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/experience_four/PrefsFragment.java
@@ -0,0 +1,12 @@
+package com.example.experience_four;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class PrefsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
